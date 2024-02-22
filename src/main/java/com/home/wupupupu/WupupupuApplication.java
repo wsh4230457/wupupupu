8767a240bfd4a443dc0b30eafbf7b3ef707cebf3
wsh <abc423457@gmail.com>
2024-02-22 20:30:51 +0800

    test

---
+++ src/main/java/com/home/wupupupu/WupupupuApplication.java
@@ -0,0 +1,13 @@
+package com.home.wupupupu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WupupupuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WupupupuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/wupupupu/WupupupuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.wupupupu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WupupupuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
